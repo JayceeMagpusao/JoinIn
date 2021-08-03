71bbe0d36b6dc0cb758f23d62f34392db9ab38ca
jaycee <jay27mag@yahoo.com>
2021-08-02 18:12:34 -0700

    test

--- frontend/join_in.jsx
+++ frontend/join_in.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  console.log("hellllloooooo")
+
   ReactDOM.render(<h1>Welcome to JoinIn!</h1>, root)
 });
