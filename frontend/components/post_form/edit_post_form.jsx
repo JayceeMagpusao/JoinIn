dd1066333d932758bebcf68d67894cddafa6d3d3
jaycee <jay27mag@yahoo.com>
2021-08-13 02:39:33 -0700

    finished up adding posts for presentation

--- frontend/components/post_form/edit_post_form.jsx
+++ frontend/components/post_form/edit_post_form.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { closeModal } from '../../actions/modal_actions';
 import { withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle, faPortrait, faThumbsUp, faPencilAlt, faTrashAlt, faEllipsisH, faLongArrowAltRight, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 class EditPostForm extends React.Component {
   constructor(props) {
@@ -29,14 +31,30 @@ class EditPostForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>body
-          <input type="text"
-            value={this.state.body}
-            onChange={this.update('body')} />
-        </label>
-        <button>Save</button>
-      </form>
+      <div className="create-post-form-container">
+        <div className="create-post-form-label-container">
+          <div className="create-post-form-label">Edit post</div>
+          <div className="x-button-icon">
+            <div onClick={() => this.props.closeModal()}>
+              <FontAwesomeIcon icon={faTimes} />
+            </div>
+          </div>
+        </div>
+        <div className="create-post-form-user">
+          <FontAwesomeIcon icon={faPortrait} />
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="create-post-input">
+            <input type="text"
+              value={this.state.body}
+              onChange={this.update('body')}
+              className="create-post-input" />
+          </div>
+          <div className="create-post-button-container">
+            <button className="create-post-form-button">Save</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
